c929224336abb7f491fb6f33bd36d4e4df729c75
Eric D. Schabell <eric@schabell.org>
2014-06-30 15:26:58 +0200

    Finished init script, now installing fuse and bpm suite.

--- init.sh
+++ init.sh
@@ -4,16 +4,20 @@ AUTHORS="Kenny Peeples, Eric D. Schabell"
 PROJECT="git@github.com:eschabell/brms-fuse-integration-demo.git"
 JBOSS_HOME=./target/jboss-eap-6.1
 FUSE_HOME=./target/jboss-fuse-6.1.0.redhat-379
+FUSE_BIN=$FUSE_HOME/bin
 SERVER_DIR=$JBOSS_HOME/standalone/deployments/
 SERVER_CONF=$JBOSS_HOME/standalone/configuration/
+SERVER_BIN=$JBOSS_HOME/bin
 SRC_DIR=./installs
 PRJ_DIR=./projects/brms-fuse-integration
+SUPPORT_DIR=./support
 FUSE=jboss-fuse-full-6.1.0.redhat-379.zip
 EAP=jboss-eap-6.1.1.zip
 BPMS=jboss-bpms-6.0.2.GA-redhat-5-deployable-eap6.x.zip
 DESIGNER=designer-patched.war
-VERSION=6.0.2.GA
-MAVENIZE_VERSION=
+BPM_VERSION=6.0.2
+FUSE_VERSION=6.1.0
+EAP_VERSION=6.1.1
 
 # wipe screen.
 clear 
@@ -65,6 +69,17 @@ else
 		exit
 fi
 
+if [[ -r $SRC_DIR/$BPMS || -L $SRC_DIR/$BPMS ]]; then
+		echo BPM Suite sources are present...
+		echo
+else
+		echo Need to download $BPMS package from the Customer Support Portal 
+		echo and place it in the $SRC_DIR directory to proceed...
+		echo
+		exit
+fi
+
+
 # Create the target directory if it does not already exist.
 if [ ! -x target ]; then
 		echo "  - creating the target directory..."
@@ -85,12 +100,12 @@ if [ -x $JBOSS_HOME ]; then
 		mv $JBOSS_HOME $JBOSS_HOME.OLD
 
 		# Unzip the JBoss EAP instance.
-		echo Unpacking JBoss EAP...
+		echo Installing JBoss EAP $EAP_VERSION
 		echo
 		unzip -q -d target $SRC_DIR/$EAP
 else
 		# Unzip the JBoss EAP instance.
-		echo Unpacking new JBoss EAP...
+		echo Installing JBoss EAP $EAP_VERSION
 		echo
 		unzip -q -d target $SRC_DIR/$EAP
 fi
@@ -105,18 +120,18 @@ if [ -x $FUSE_HOME ]; then
 		mv $FUSE_HOME $FUSE_HOME.OLD
 
 		# Unzip the JBoss FUSE instance.
-		echo Unpacking JBoss FUSE...
+		echo Installing JBoss FUSE $FUSE_VERSION
 		echo
 		unzip -q -d target $SRC_DIR/$FUSE
 else
 		# Unzip the JBoss FUSE instance.
-		echo Unpacking new JBoss FUSE...
+		echo Installing JBoss FUSE $FUSE_VERSION
 		echo
 		unzip -q -d target $SRC_DIR/$FUSE
 fi
 
 # Unzip the required files from JBoss product deployable.
-echo Unpacking $PRODUCT $VERSION...
+echo Installing JBoss BPM Suite $BPM_VERSION
 echo
 unzip -q -o -d target $SRC_DIR/$BPMS
 
@@ -155,8 +170,12 @@ echo
 cd $PRJ_DIR
 mvn clean install 
 
-echo "You can now start the $PRODUCT with $SERVER_BIN/standalone.sh"
 echo
+echo
+echo "You can now start the JBoss BPM Suite with $SERVER_BIN/standalone.sh"
+echo
+
+echo "You can start the JBoss Fuse with $FUSE_BIN/start"
 
 echo "$PRODUCT $VERSION $DEMO Setup Complete."
 echo
